dfeef1abf9e18f47e5cdbef0ff5b53dc3d95e87d
omar0425 <startfresh87@gmail.com>
2022-07-19 23:09:58 -0400

    working on state to update forms for post

--- src/components/NewSuperHeroForm.js
+++ src/components/NewSuperHeroForm.js
@@ -1,36 +1,53 @@
 import React,{useState} from "react";
 
 export default function NewSuperHeroForm({addSuperHero}){
-
+const[formData,setFormData] = useState({
+  "id": "",
+  "name": "",
+  "url": "",
+  "firstappearance": "",
+  "publisher": ""
+})
+console.log(formData)
+function handleChange(event){
+  setFormData(prevFormData =>{
+    return {...prevFormData,[event.target.name]: event.target.value
+  }})
+}
  
   return(
     <div className="new-superhero-form">
       <h2>Add Your Own Super Hero 🦸‍♀️ </h2>
       <form onSubmit={()=>{console.log(`submitting form`)}}>
+
        
         <input 
         type="text" 
         name="name" 
-        placeholder="SuperHero name" 
-        // onChange={handleChange} 
+        placeholder="Super Hero name" 
+        value ={formData.superHeroName}
+        onChange={handleChange} 
         />
         <input 
         type="text" 
         name="image" 
         placeholder="Image url" 
-        // onChange={handleChange}
+        value={formData.url}
+        onChange={handleChange}
         />
         <input 
         type="text" 
         name="firstappearance" 
         placeholder="First Appearance" 
-        // onChange={handleChange}
+        value={formData.firstappearance}
+        onChange={handleChange}
         />
         <input 
         type="text" 
         name="publisher" 
         placeholder="Publisher" 
-        // onChange={handleChange}
+         value={formData.publisher}
+        onChange={handleChange}
         />
         
         <button type="submit">Add SuperHero</button>
